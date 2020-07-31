218b00b7727c06e6363c1492e3526128e153486f
Megalomaniac Schtkan <texview@yandex.ru>
2020-07-31 23:30:22 +0300

    Added images to resources (for SwitchList)

--- DeviceTuner/Modules/DeviceTuner.Modules.ModuleName/ViewModels/DesignSwitchConverter.cs
+++ DeviceTuner/Modules/DeviceTuner.Modules.ModuleName/ViewModels/DesignSwitchConverter.cs
@@ -7,24 +7,35 @@ using System.Text;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Media.TextFormatting;
 
 namespace DeviceTuner.Modules.ModuleSwitch.ViewModels
 {
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
     public class DesignSwitchConverter : IValueConverter
     {
+        public BitmapImage ImagePathOK { get; set; }
+        public BitmapImage ImagePathCancel { get; set; }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            
             if (value is string)
             {
                 bool tmp = false;
                 string str = (string)value;
+
                 if (str.Length > 0) tmp = true;
-                return (tmp) || DesignerProperties.GetIsInDesignMode(new DependencyObject())
+                return tmp || DesignerProperties.GetIsInDesignMode(new DependencyObject())
+                    ? ImagePathOK
+                    : ImagePathCancel;
+                /*return (tmp) || DesignerProperties.GetIsInDesignMode(new DependencyObject())
                     ? new SolidColorBrush(Colors.Green)
                     : new SolidColorBrush(Colors.Red);
+                */
             }
-            return new SolidColorBrush(Colors.Aqua);
+            return ImagePathCancel;//new SolidColorBrush(Colors.Aqua);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
